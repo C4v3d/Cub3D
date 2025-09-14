e6566f1c163f7ef20458c0715fa866812ecd3880
Theo Immi <timmi@c1r1s6.42lausanne.ch>
2025-09-14 19:09:25 +0200

    Map validation WIP

--- include/validator.h
+++ include/validator.h
@@ -6,15 +6,15 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 17:23:22 by timmi             #+#    #+#             */
-/*   Updated: 2025/09/14 17:46:44 by timmi            ###   ########.fr       */
+/*   Updated: 2025/09/14 17:57:54 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VALIDATOR_H
 # define VALIDATOR_H
 
-# define EXT ".cub"
-# define EXT_LEN 4
+# define EXTENSION ".cub"
+# define EXTENSION_LEN 4
 
 bool	is_map_valid(char *map_path);
 

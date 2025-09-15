3fd41e8fa3bf7080aa46e549a199f05d191e21d9
eriotto-momomo <eliot.monachon@proton.me>
2025-09-15 11:46:57 +0200

    added game structs

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: emonacho <emonacho@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 11:13:57 by timmi             #+#    #+#             */
-/*   Updated: 2025/09/14 19:12:56 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/09/15 11:45:13 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,17 @@
 # include "error.h"
 # include "gamedata.h"
 # include "../lib/libft/libft.h"
+
+typedef struct		s_map_data;
+typedef struct		s_player_data;
+typedef struct		s_graphic_data;
+
+typedef struct		s_game_data
+{
+	t_player_data	*p;
+	t_map_data		*map;
+	t_graphic_data	*gfx;
+	//🚧ptr to main struct
+}					t_game_data;
 
 #endif
--- include/gamedata.h
+++ include/gamedata.h
@@ -3,16 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   gamedata.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: emonacho <emonacho@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 19:13:03 by emonacho          #+#    #+#             */
-/*   Updated: 2025/09/14 19:14:06 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/09/15 11:45:39 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GAMEDATA_H
 # define GAMEDATA_H
 
+# define ESC 65307
+# define Q 113
+# define W 119
+# define A 97
+# define S 115
+# define D 100
 
+typedef struct		s_player_data
+{
+	int				x;
+	int				y;
+	t_game_data		*game;
+}					t_player_data;
+
+typedef struct		s_map_data
+{
+	int				**grid;		// MAP MATRIX
+	int				*start_pos;	// PLAYER X&Y START POSITION + ORIENTATION (N,S,W or E)
+	t_game_data		*game;
+}					t_map_data;
+
+typedef struct		s_graphic_data
+{
+	void			*no_texture;// NORTH
+	void			*so_texture;// SOUTH
+	void			*we_texture;// WEST
+	void			*ea_texture;// EAST
+	int				*f_color;	// FLOOR in RGB
+	int				*c_color;	// CEILINIG in RGB
+	t_game_data		*game;
+}					t_graphic_data;
 
 #endif

2f75fd798a40b0a77c76dfdf1f8d977e8ee26f9e
Eliot Monachon <emonacho@c1r7s8.42lausanne.ch>
2025-09-18 13:43:22 +0200

    wip

--- include/checker.h
+++ include/checker.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   checker.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emonacho <emonacho@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 17:23:22 by timmi             #+#    #+#             */
-/*   Updated: 2025/09/16 22:56:29 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/09/18 13:38:48 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 # define EXTENSION ".cub"
 # define EXTENSION_LEN 4
-
-# include "cubstructs.h"
 
 bool	is_map_valid(t_main *cub, char *map_path);
 

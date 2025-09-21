7f59e83f61867c2ee26df0eadd91a6d89adbfb66
Eriotto Momomo <productivity.croak732@passmail.net>
2025-09-21 17:58:33 +0200

    Revert "Added WASD keys inputs to move player on tiles & map"

--- src/loop/loop.c
+++ src/loop/loop.c
@@ -1,19 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   position_moves.c                                   :+:      :+:    :+:   */
+/*   loop.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emonacho <emonacho@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/17 23:04:33 by emonacho          #+#    #+#             */
-/*   Updated: 2025/09/18 12:01:08 by emonacho         ###   ########.fr       */
+/*   Created: 2025/09/18 12:36:48 by emonacho          #+#    #+#             */
+/*   Updated: 2025/09/18 12:38:46 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-/*
-* -	Déplacement du jouer avec WASD
-* -	Comment gérer deux inputs clavier?
-* -	Et du coup gérer les déplacements en diagonale?
-*/
+

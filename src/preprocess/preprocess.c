847575825e6298f8393b246cb2b32128b2c21a1b
Theo Immi <timmi@c1r13s3.42lausanne.ch>
2025-10-02 14:34:52 +0200

    WIP map validation

--- src/preprocess/preprocess.c
+++ src/preprocess/preprocess.c
@@ -6,20 +6,20 @@
 /*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 14:35:10 by timmi             #+#    #+#             */
-/*   Updated: 2025/10/02 13:53:19 by timmi            ###   ########.fr       */
+/*   Updated: 2025/10/02 14:13:04 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
 /**
- * Main function for the preprocess
- * Wrapper all the function that
- * are needed in the preprocess phase
+ * Main function for the preprocess.
+ * Wrap all the function that
+ * are needed in the preprocess phase.
  * 
- * Preprocess phase aims to prepare everything
+ * Preprocess phase task is to prepare everything
  * for the execution phase.
- * easy management and debugging since everything
+ * Easy management and debugging since everything
  * is broken down into separeted function !
  * 
  * Break down the main struct into 2 separeted
@@ -32,11 +32,9 @@ void	preprocess(t_main *cub, char *input_file)
 {
 	if (is_input_valid(input_file) == false)
 		exit(1);
-	fprintf(stderr, GRN"Input string valid !\n"RESET);
 	init_cub(cub);
-	fprintf(stderr, GRN"Cub initialized\n"RESET);
 	open_file(cub, input_file);
-	fprintf(stderr, GRN"File opened\n"RESET);
+	/*Check if map is before data*/
 	parse_file(cub);
 	// validate_map(cub->map);
 	init_parsed_data(cub);

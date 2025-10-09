44cc68e76be624a3f1f5cc50e537112f2edda82a
Eliot Monachon <emonacho@c1r9s5.42lausanne.ch>
2025-10-09 16:44:33 +0200

    updated vision moves

--- include/maths_utils.h
+++ include/maths_utils.h
@@ -6,13 +6,14 @@
 /*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 16:46:24 by emonacho          #+#    #+#             */
-/*   Updated: 2025/10/04 17:25:00 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/10/09 16:40:45 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATHS_UTILS_H
 # define MATHS_UTILS_H
 
-double	degrees_to_radians(t_player *p);
+double	degrees_to_radians(double degrees);
+void	get_cos_sin(t_player *p);
 
 #endif

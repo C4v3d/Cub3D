9455b79158a20fec985800cae2dd1e7b34184677
Eliot Monachon <emonacho@c1r9s5.42lausanne.ch>
2025-10-09 19:45:17 +0200

    movements seems to works properly, cleaned some unused struct and variables

--- src/moves/position_moves.c
+++ src/moves/position_moves.c
@@ -6,7 +6,7 @@
 /*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 23:04:33 by emonacho          #+#    #+#             */
-/*   Updated: 2025/10/09 18:10:10 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/10/09 19:43:47 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ static bool	new_pos(t_player *p, int kc)
 	last_pos_y = p->pos[Y];
 	if (kc == W)
 	{
-		p->pos[X] -= p->trgo.cos_a * MOVE_UNIT;
+		p->pos[X] += p->trgo.cos_a * MOVE_UNIT;
 		p->pos[Y] -= p->trgo.sin_a * MOVE_UNIT;
 	}
 	if (kc == S)
 	{
-		p->pos[X] += p->trgo.cos_a * MOVE_UNIT;
+		p->pos[X] -= p->trgo.cos_a * MOVE_UNIT;
 		p->pos[Y] += p->trgo.sin_a * MOVE_UNIT;
 	}
 	if ((p->pos[X] <= 0 || p->pos[X] >= p->cub->map.dim[X])
@@ -47,6 +47,6 @@ int	update_plyr_position(t_player *p, int kc)
 	get_cos_sin(p);
 	if (!new_pos(p, kc))
 		return (fprintf(stderr, "%supdate_plr_position | CAN'T GO THERE%s\n", RED, RESET), 0);
-	fprintf(stderr, "| %supdate_plyr_position%s\n| aov: %f\n| pos[X]: %f\n| pos[Y]: %f\n", BLU, RESET, p->aov, p->pos[X], p->pos[Y]);
+	fprintf(stderr, "| %supdate_plyr_position%s\n| aov: %lf\n| pos[X]: %f\n| pos[Y]: %f\n", BLU, RESET, p->aov, p->pos[X], p->pos[Y]);
 	return (0);
 }

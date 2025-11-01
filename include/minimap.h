54e50f7f6ef1b49f97d5cdb5ce830f011281f271
eriotto-momomo <eliot.monachon@proton.me>
2025-11-01 18:02:47 +0100

    modification de minimap pour la rendre plus generique

--- include/minimap.h
+++ include/minimap.h
@@ -6,7 +6,7 @@
 /*   By: emonacho <emonacho@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 14:13:28 by emonacho          #+#    #+#             */
-/*   Updated: 2025/10/26 17:58:25 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/11/01 17:45:19 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 typedef struct	s_image_data t_image;
 
-void	draw_minimap(t_main *cub);
-void	mark_minimap(t_image *map, t_player *p);
+void	draw_minimap(t_main *cub, t_image *img);
+void	mark_minimap(t_image *img, t_player *p);
 
 #endif
--- src/minimap/minimap_rendering.c
+++ src/minimap/minimap_rendering.c
@@ -6,18 +6,18 @@
 /*   By: emonacho <emonacho@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 14:12:43 by emonacho          #+#    #+#             */
-/*   Updated: 2025/10/30 18:12:58 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/11/01 17:46:36 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-static void	draw_line(t_image *map, t_player *p)
+static void	draw_line(t_image *img, t_player *p)
 {
-	my_mlx_pixel_put(map, (p->pos[X] + p->dir[X]) * MINI_RES, (p->pos[Y] - p->dir[Y]) * MINI_RES, 0xFFFFFF);
+	my_mlx_pixel_put(img, (p->pos[X] + p->dir[X]) * MINI_RES, (p->pos[Y] - p->dir[Y]) * MINI_RES, 0xFFFFFF);
 }
 
-static void	draw_tile(t_image *map, size_t x_pos, size_t y_pos, int color)
+static void	draw_tile(t_image *img, size_t x_pos, size_t y_pos, int color)
 {
 	size_t	x_i;
 	size_t	y_i;
@@ -28,11 +28,11 @@ static void	draw_tile(t_image *map, size_t x_pos, size_t y_pos, int color)
 	{
 		x_i = 0;
 		while (x_i++ < MINI_RES)
-			my_mlx_pixel_put(map, (x_pos + x_i), (y_pos + y_i), color);
+			my_mlx_pixel_put(img, (x_pos + x_i), (y_pos + y_i), color);
 	}
 }
 
-static void	draw_plyr(t_image *map, double pos_x, double pos_y, double dir[2], int color)
+static void	draw_plyr(t_image *img, double pos_x, double pos_y, double dir[2], int color)
 {
 	size_t	x_i;
 	size_t	y_i;
@@ -43,11 +43,11 @@ static void	draw_plyr(t_image *map, double pos_x, double pos_y, double dir[2], i
 	{
 		x_i = 0;
 		while (x_i++ < MINI_RES)
-			my_mlx_pixel_put(map, ((pos_x - 8) + x_i), ((pos_y - 8) + y_i), color);
+			my_mlx_pixel_put(img, ((pos_x - 8) + x_i), ((pos_y - 8) + y_i), color);
 	}
 }
 
-void	draw_minimap(t_main *cub)
+void	draw_minimap(t_main *cub, t_image *img)
 {
 	size_t	x_i;
 	size_t	y_i;
@@ -59,12 +59,12 @@ void	draw_minimap(t_main *cub)
 		while (cub->map.grid[y_i][x_i])
 		{
 			if (cub->map.grid[y_i][x_i] == '1')
-				draw_tile(&cub->gfx.map, x_i * MINI_RES, y_i * MINI_RES, 200);
+				draw_tile(img, x_i * MINI_RES, y_i * MINI_RES, 200);
 			x_i++;
 		}
-		draw_plyr(&cub->gfx.map, cub->plyr.pos[X] * MINI_RES, cub->plyr.pos[Y] * MINI_RES, cub->plyr.dir, 0xFF0000);
-		draw_line(&cub->gfx.map, &cub->plyr);
+		draw_plyr(img, cub->plyr.pos[X] * MINI_RES, cub->plyr.pos[Y] * MINI_RES, cub->plyr.dir, 0xFF0000);
+		draw_line(img, &cub->plyr);
 		y_i++;
 	}
-	mark_minimap(&cub->gfx.map, &cub->plyr);
+	mark_minimap(img, &cub->plyr);
 }

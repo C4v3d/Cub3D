028ffc1955320637c39b77a2581e3d3d7894d24a
Theo Immi <timmi@c1r7s2.42lausanne.ch>
2025-11-21 11:40:03 +0100

    Minimap no longer crash

--- include/minimap.h
+++ include/minimap.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   minimap.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emonacho <emonacho@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 14:13:28 by emonacho          #+#    #+#             */
-/*   Updated: 2025/11/01 17:45:19 by emonacho         ###   ########.fr       */
+/*   Updated: 2025/11/21 11:38:48 by timmi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIMAP_H
 # define MINIMAP_H
 
-# define MINI_RES 12
+# define MINI_RES 8 /* SWEET SPOT*/
 
 typedef struct	s_image_data t_image;
 

b78b9a2bec8d46c015eeab2fcc3fca5eff64eed9
Theo Immi <timmi@c1r7s2.42lausanne.ch>
2025-11-25 15:50:23 +0100

    corrected

---
+++ include/fw.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fw.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 15:48:19 by timmi             #+#    #+#             */
+/*   Updated: 2025/11/25 15:49:47 by timmi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FW_H
+# define FW_H
+
+# define AXIS 2
+
+typedef struct s_img				t_img;
+typedef struct s_main_struct		t_main;
+typedef struct s_program_data		t_prog;
+typedef struct s_player_data		t_player;
+typedef struct s_scene				t_scene;
+typedef struct s_graphic_data		t_graphic;
+typedef struct s_rays_calculation	t_rays;
+typedef struct s_map_data			t_map;
+typedef union u_color				t_color;
+
+#endif

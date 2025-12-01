50de5a115d3094dc12d81a2ad2f0c2e0c08f92b4
Eliot Monachon <emonacho@c1r7s4.42lausanne.ch>
2025-12-01 11:50:54 +0100

    fixed segfault on closing window by clicking cross

--- include/utils.h
+++ include/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: timmi <timmi@student.42.fr>                +#+  +:+       +#+        */
+/*   By: emonacho <emonacho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 16:46:24 by emonacho          #+#    #+#             */
-/*   Updated: 2025/11/25 15:17:12 by timmi            ###   ########.fr       */
+/*   Updated: 2025/12/01 11:47:23 by emonacho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 double	get_delta(double val);
 void	welcome_cub3d(void);
+int		close_window(t_main *cub);
 
 #endif
